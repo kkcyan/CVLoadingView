7ba70f102118e410c2828472f8b80b42bc7b2c5d
范玉杰 <1072429605@qq.com>
2017-03-31 14:21:27 +0800

    Initial Commit

---
+++ CVLoadingView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CVLoadingView
+//
+//  Created by 范玉杰 on 2017/3/31.
+//  Copyright © 2017年 范玉杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
