427471fc280a275bd56889d4a8b1408f0765245a
范玉杰 <1072429605@qq.com>
2017-03-31 14:49:33 +0800

    CVLoadingView
    加载框

--- CVLoadingView/ViewController.swift
+++ CVLoadingView/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var start: UIButton!
+    @IBOutlet weak var stop: UIButton!
+    
+    weak var loadingView: LoadingView!
+    
+    @IBAction func clickStart(_ sender: UIButton) {
+        
+        starLoadingView()
+        start.isEnabled = false
+        stop.isEnabled = true
+    }
+    
+    @IBAction func clickStop(_ sender: UIButton) {
+        
+        loadingView.hideLoadingView()
+        start.isEnabled = true
+        stop.isEnabled = false
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        starLoadingView()
+        
+        self.start.isEnabled = false
+        self.stop.isEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +46,13 @@ class ViewController: UIViewController {
     }
 
 
+    func starLoadingView(){
+        
+        print("开始了")
+        
+        let loadingView = LoadingView.showLoadingWithView(view: self.view)
+        
+        self.loadingView = loadingView
+    }
 }
 
